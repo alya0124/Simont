90be9d54b22417e0bbd7b22f347376a81f1c70d1
alya0124 <23301542@uttt.edu.mx>
2024-08-08 00:58:50 -0600

    0.0119 DESPLIEGUE EN RENDER

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=443)
+    port = int(os.environ.get('PORT', 5000))  # Usa el puerto proporcionado por Render en producción
+    app.run(host='0.0.0.0', port=port)
